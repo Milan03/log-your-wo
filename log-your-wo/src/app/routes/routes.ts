a87c86b9dad391353a2033da6b4593b8cfe2af79
Milan Sobat <milansobat03@gmail.com>
2020-05-23 18:21:37 -0400

    Removed: unused route

--- log-your-wo/src/app/routes/routes.ts
+++ log-your-wo/src/app/routes/routes.ts
@@ -9,8 +9,7 @@ export const routes: Routes = [
         children: [
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
-            { path: 'log-entry', loadChildren: () => import('./log-entry/log-entry.module').then(m => m.LogEntryModule) },
-            { path: 'material', loadChildren: () => import('./material/material.module').then(m => m.MaterialModule) }
+            { path: 'log-entry', loadChildren: () => import('./log-entry/log-entry.module').then(m => m.LogEntryModule) }
         ]
     },
 
