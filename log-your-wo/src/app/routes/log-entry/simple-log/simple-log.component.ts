3441239f20b0063262ad30a368d635a6db06bdc4
Milan Sobat <milansobat03@gmail.com>
2020-05-25 09:41:34 -0400

    Added: form/form group to html/ts

--- log-your-wo/src/app/routes/log-entry/simple-log/simple-log.component.ts
+++ log-your-wo/src/app/routes/log-entry/simple-log/simple-log.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 import { SharedService } from '../../../shared/services/shared.service';
 
@@ -11,13 +12,15 @@ import { SimpleLog } from '../../../shared/models/simple-log.model';
     styleUrls: ['./simple-log.component.scss']
 })
 export class SimpleLogComponent implements OnInit {
+    private simpleLogForm: FormGroup;
     private currentLog: SimpleLog;
     private logStartDatim: Date;
 
     constructor(
+        private _formBuilder: FormBuilder,
         private _sharedService: SharedService
     ) {
-
+        this.simpleLogForm = _formBuilder.group({});
     }
 
     ngOnInit(): void {
@@ -25,6 +28,16 @@ export class SimpleLogComponent implements OnInit {
         this.logStartDatim = new Date();
         this._sharedService.emitLogType(LogTypes.SimpleLog);
         this._sharedService.emitLogStartDatim(this.logStartDatim);
+    }
+
+    private submitForm($ev, value: any): void {
+        $ev.preventDefault();
+        for (let c in this.simpleLogForm.controls) {
+            this.simpleLogForm.controls[c].markAsTouched();
+        }
+        if (this.simpleLogForm.valid) {
+            
+        }
     }
 
 }
