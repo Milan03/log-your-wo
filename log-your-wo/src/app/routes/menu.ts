d0a28258e437a9ad5d30de07c6ab37ace9053ddf
Milan Sobat <milansobat03@gmail.com>
2020-07-06 18:39:07 -0400

    Added: sidebar translations

--- log-your-wo/src/app/routes/menu.ts
+++ log-your-wo/src/app/routes/menu.ts
@@ -2,18 +2,21 @@
 const Home = {
     text: 'Home',
     link: '/home',
-    icon: 'icon-home'
+    icon: 'icon-home',
+    translate: 'global.Home'
 };
 
 const SimpleLog = {
     text: 'Simple Log',
     link: '/log-entry/simple-log',
-    icon: 'icon-notebook'
+    icon: 'icon-notebook',
+    translate: 'log-entry.SimpleLog'
 };
 
 const headingMain = {
     text: 'Main Navigation',
-    heading: true
+    heading: true,
+    translate: 'global.MainNavigation'
 };
 
 export const menu = [
