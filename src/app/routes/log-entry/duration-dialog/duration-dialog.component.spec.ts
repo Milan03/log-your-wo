3286b5bf53fe00a8202d817e17c3d55480b35e6a
Milan Sobat <milansobat03@gmail.com>
2023-12-09 10:28:42 -0500

    Updated: angular@10->@11

--- src/app/routes/log-entry/duration-dialog/duration-dialog.component.spec.ts
+++ src/app/routes/log-entry/duration-dialog/duration-dialog.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DurationDialogComponent } from './duration-dialog.component';
 
@@ -6,7 +6,7 @@ describe('DurationDialogComponent', () => {
   let component: DurationDialogComponent;
   let fixture: ComponentFixture<DurationDialogComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DurationDialogComponent ]
     })
--- src/app/routes/log-entry/simple-log/simple-log.component.spec.ts
+++ src/app/routes/log-entry/simple-log/simple-log.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SimpleLogComponent } from './simple-log.component';
 
@@ -6,7 +6,7 @@ describe('SimpleLogComponent', () => {
   let component: SimpleLogComponent;
   let fixture: ComponentFixture<SimpleLogComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SimpleLogComponent ]
     })
